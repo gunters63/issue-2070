c06572704878e7827adaf75b6b59ff85f4578441
Gunter Spranz <gunter@gspranz.de>
2022-09-23 07:09:04 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { default as Keyboard } from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Keyboard/>
+    </div>
+  );
+}
+
+export default App;
